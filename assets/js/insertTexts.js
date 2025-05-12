ae695dd306a33028d22bbab1e3556043a043b710
roginaro <rogicavalcante@gmail.com>
2025-05-12 11:28:19 +0100

    consolelog lang

--- assets/js/insertTexts.js
+++ assets/js/insertTexts.js
@@ -1,17 +1,19 @@
 
 fetch('../assets/js/langPt.json')
-      .then(response => response.json())
-      .then(lang => {
-        document.querySelectorAll('[data-i18n]').forEach(el => {
-          const path = el.getAttribute('data-i18n').split('.');
-          let value = lang;
-          for (const key of path) {
-            value = value?.[key];
-            if (value === undefined) break;
-          }
-          if (value !== undefined) {
-            el.textContent = value;
-          }
-        });
-      })
-      .catch(error => console.error('Erro ao carregar JSON:', error));
+    .then(response => response.json())
+    .then(lang => {
+        console.log(lang)
+        document.querySelectorAll('[data-i18n]').forEach(el => {
+            const path = el.getAttribute('data-i18n').split('.');
+            let value = lang;
+            for (const key of path) {
+                value = value?.[key];
+                if (value === undefined) break;
+                7
+            }
+            if (value !== undefined) {
+                el.textContent = value;
+            }
+        });
+    })
+    .catch(error => console.error('Erro ao carregar JSON:', error));
