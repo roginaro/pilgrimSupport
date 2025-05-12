9a22b91e346fd2e4a966812aff5c5f95a9c23893
roginaro <rogicavalcante@gmail.com>
2025-05-12 11:33:26 +0100

    ajustre

--- assets/js/insertTexts.js
+++ assets/js/insertTexts.js
@@ -2,14 +2,12 @@
 fetch('../assets/js/langPt.json')
     .then(response => response.json())
     .then(lang => {
-        console.log(lang)
         document.querySelectorAll('[data-i18n]').forEach(el => {
             const path = el.getAttribute('data-i18n').split('.');
             let value = lang;
             for (const key of path) {
                 value = value?.[key];
                 if (value === undefined) break;
-                7
             }
             if (value !== undefined) {
                 el.textContent = value;
